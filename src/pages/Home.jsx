a48c8b305bbcb639564eeb6869c43136e1d09ff0
IsrPort <isra.pqr@gmail.com>
2023-09-27 15:42:45 -0400

    Se agrego semantic UI y configuracion de rutas para el Home

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import { Button } from 'semantic-ui-react'
+
+
+export default function Home () {
+    return (
+      <div>
+        <h1>Home Principal</h1>
+        <Button primary>Primary</Button>
+      </div>
+    )
+  }
