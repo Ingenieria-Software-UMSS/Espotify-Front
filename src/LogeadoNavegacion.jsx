6f5f1b0844dbc3345384f3868a47cee3c82a0656
Lisandro.vargas <jose.vargas.60106@usalesiana.edu.bo>
2023-10-02 19:49:10 -0400

    Rebase continue

--- src/LogeadoNavegacion.jsx
+++ src/LogeadoNavegacion.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import LogeadoLayout from './layouts/LogeadoLayout'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
+import Informacion from "./components/Informacion";
 
 const LogeadoNavegacion = () => {
   return (
@@ -10,7 +11,9 @@ const LogeadoNavegacion = () => {
       <LogeadoLayout>
         <Routes>
           <Route path='/' element={<Home />} />
+            <Route path="cancion" element={<Informacion />} />
         </Routes>
+
       </LogeadoLayout>
 
     </BrowserRouter>
