83d8034533522b4313b170ffc66b3edd5f9888c6
Lisandro.vargas <jose.vargas.60106@usalesiana.edu.bo>
2023-10-04 14:26:02 -0400

    Add funcionality into the buttons

--- src/components/ListaCanciones.jsx
+++ src/components/ListaCanciones.jsx
@@ -1,17 +1,14 @@
-import React from 'react'
-import { Table, Icon, Button } from "semantic-ui-react"
+import React, { useState } from 'react';
+import { Table, Icon, Button } from "semantic-ui-react";
 import "./ListaCanciones.css";
 import { UsarPlayer } from '../hooks/UsarPlayer';
 import { useNavigate } from 'react-router-dom';
 
-
 const ListaCanciones = (props) => {
     const { canciones } = props;
     const navigate = useNavigate();
-
     const { playCancion, play } = UsarPlayer();
-
-    console.log(play);
+    const [isLoNuevoSelected, setIsLoNuevoSelected] = useState(true);
 
     const onClick = (i) => () => {
         const song = canciones[i];
@@ -29,58 +26,68 @@ const ListaCanciones = (props) => {
     return (
         <div className='list_songs_container' >
             <div className='list_songs__buttons'>
-                <Button>Lo nuevo</Button>
-                <Button inverted>Ultimos Artistas</Button>
+                <Button
+                    onClick={() => setIsLoNuevoSelected(true)}
+                    inverted={!isLoNuevoSelected}
+                >
+                    Lo nuevo
+                </Button>
+                <Button
+                    onClick={() => setIsLoNuevoSelected(false)}
+                    inverted={isLoNuevoSelected}
+                >
+                    Ultimos Artistas
+                </Button>
             </div>
 
             <div className='tabla'>
-                <Table inverted className='lista-canciones' >
-                    <Table.Header>
-                        <Table.Row>
-                            <Table.HeaderCell>#</Table.HeaderCell>
-                            <Table.HeaderCell></Table.HeaderCell>
-                            <Table.HeaderCell>Título</Table.HeaderCell>
-                            <Table.HeaderCell>Artista</Table.HeaderCell>
-                            <Table.HeaderCell>Duración</Table.HeaderCell>
-                            <Table.HeaderCell>Acción</Table.HeaderCell>
-                        </Table.Row>
-                    </Table.Header>
-
-                    <Table.Body>
-                        {[...canciones].map((cancion, i) => (
-
-                            <Table.Row onClick={onClick(i)} key={i}>
-                                <Table.Cell>
-                                    {i + 1}
-                                </Table.Cell>
-                                <Table.Cell>
-                                    <img src={cancion.urlImagen} className='miniatura'></img>
-
-                                </Table.Cell>
-                                <Table.Cell>
-                                    {cancion.nombre}
-                                </Table.Cell>
-                                <Table.Cell>
-                                    {cancion.artista}
-                                </Table.Cell>
-                                <Table.Cell>
-                                    {cancion.duracion}
-                                </Table.Cell>
-                                <Table.Cell onClick={(ev) => {
-                                    ev.stopPropagation();
-                                    onPlay(cancion)
-                                }}>
-                                    <Icon size='large' name='play circle outline' />
-                                </Table.Cell>
+                {isLoNuevoSelected ? (
+                    <Table inverted className='lista-canciones' >
+                        <Table.Header>
+                            <Table.Row>
+                                <Table.HeaderCell>#</Table.HeaderCell>
+                                <Table.HeaderCell></Table.HeaderCell>
+                                <Table.HeaderCell>Título</Table.HeaderCell>
+                                <Table.HeaderCell>Artista</Table.HeaderCell>
+                                <Table.HeaderCell>Duración</Table.HeaderCell>
+                                <Table.HeaderCell>Acción</Table.HeaderCell>
                             </Table.Row>
-                        ))}
-                    </Table.Body>
-                </Table>
+                        </Table.Header>
+
+                        <Table.Body>
+                            {[...canciones].map((cancion, i) => (
+                                <Table.Row onClick={onClick(i)} key={i}>
+                                    <Table.Cell>
+                                        {i + 1}
+                                    </Table.Cell>
+                                    <Table.Cell>
+                                        <img src={cancion.urlImagen} className='miniatura'></img>
+                                    </Table.Cell>
+                                    <Table.Cell>
+                                        {cancion.nombre}
+                                    </Table.Cell>
+                                    <Table.Cell>
+                                        {cancion.artista}
+                                    </Table.Cell>
+                                    <Table.Cell>
+                                        {cancion.duracion}
+                                    </Table.Cell>
+                                    <Table.Cell onClick={(ev) => {
+                                        ev.stopPropagation();
+                                        onPlay(cancion)
+                                    }}>
+                                        <Icon size='large' name='play circle outline' />
+                                    </Table.Cell>
+                                </Table.Row>
+                            ))}
+                        </Table.Body>
+                    </Table>
+                ) : (
+                    <p>No se encontraron canciones</p>
+                )}
             </div>
-
-
         </div>
     )
 }
 
-export default ListaCanciones
+export default ListaCanciones;
