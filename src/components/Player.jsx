736bbf9a4fd2f835acd027bd48c4efe8989c5334
Lisandro.vargas <jose.vargas.60106@usalesiana.edu.bo>
2023-10-11 20:06:16 -0400

    Add event when end music and clean code comment

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,95 +1,71 @@
-import React, { useEffect, useState } from 'react'
-import { Icon, Progress } from "semantic-ui-react"
-import ReactPlayer from 'react-player'
-import { UsarPlayer } from '../hooks/UsarPlayer'
-import "./Player.css"
-import { set } from 'lodash'
+import React, { useEffect, useState } from 'react';
+import { Icon, Progress } from 'semantic-ui-react';
+import ReactPlayer from 'react-player';
+import { UsarPlayer } from '../hooks/UsarPlayer';
+import './Player.css';
+import { set } from 'lodash';
 
 const Player = (props) => {
-
-
   const { cancion, play, pause, resume, volumen } = UsarPlayer();
-  //const playing = false;
-  const [totalSeconds, setTotalSeconds] = useState(0)
+  const [totalSeconds, setTotalSeconds] = useState(0);
   const [currentSeconds, setCurrentSeconds] = useState(0);
-
-  const [showTotalSeconds, setShowTotalSeconds] = useState(0)
+  const [showTotalSeconds, setShowTotalSeconds] = useState(0);
   const [showCurrentSeconds, setShowCurrentSeconds] = useState(0);
-
   const [totalCancionDB, setTotalCancionDB] = useState('00:00');
-  const [bloqueo,SetBloqueo] = useState(true);
-  //console.log(cancion);
-  
+  const [bloqueo, SetBloqueo] = useState(true);
+
   useEffect(() => {
-
-    setShowCurrentSeconds(formatear(currentSeconds))
+    setShowCurrentSeconds(formatear(currentSeconds));
     setShowTotalSeconds(formatear(totalSeconds));
-    
-  })
+  });
 
-  //console.log(play);
   const enProgreso = (data) => {
-
-    // const segundos = (Math.round(data.playedSeconds % 0x3C)).toString();
-    // const minutos  = (Math.floor(data.playedSeconds / 0x3C ) % 0x3C).toString();
-    // let nuevoCurrent = minutos + ':'+ segundos;
-    // //console.log(nuevo)
-    // const segundosTotal = (Math.round(data.loadedSeconds % 0x3C)).toString();
-    // const minutosTotal  = (Math.floor(data.loadedSeconds / 0x3C ) % 0x3C).toString();
-    // let nuevoTotal = '0'+minutosTotal + ':'+ segundosTotal;
-    // setShowCurrentSeconds(nuevoCurrent);
-    // setShowTotalSeconds(nuevoTotal);
-
-
-    // setTotalSeconds(data.loadedSeconds);
-    // setCurrentSeconds(data.playedSeconds);
-
-
-    // setShowCurrentSeconds(formatear(data.playedSeconds));
-    // setShowTotalSeconds(formatear(data.loadedSeconds));
     setTotalSeconds(data.loadedSeconds);
     setCurrentSeconds(data.playedSeconds);
-
     obtainTotalCancion();
     desbloquearBotonPlay();
-    //console.log(data);
-  }
+  };
 
-  const obtainTotalCancion= ()=>{
+  const obtainTotalCancion = () => {
     setTotalCancionDB(cancion.duracion);
-  }
+  };
 
-  const desbloquearBotonPlay = ()=>{
+  const desbloquearBotonPlay = () => {
     SetBloqueo(false);
-  }
+  };
 
   const formatear = (tiempo) => {
-
     const minutos = Math.floor(tiempo / 60) < 10 ? `0${Math.floor(tiempo / 60)}` : Math.floor(tiempo / 60);
     const segundos = Math.floor(tiempo % 60) < 10 ? `0${Math.floor(tiempo % 60)}` : Math.floor(tiempo % 60);
+    return `${minutos}:${segundos}`;
+  };
 
-    return `${minutos}: ${segundos}`;
-
+  const handleEnded = () => {
+    pause();
+    SetBloqueo(false);
   };
 
   return (
-    <div className='player'>
-      <Icon name={play ? "pause circle outline" : "play circle outline"} onClick={play ? pause : resume} disabled={bloqueo}/>
-      <div className='progress'>
+      <div className="player">
+        <Icon name={play ? 'pause circle outline' : 'play circle outline'} onClick={play ? pause : resume} disabled={bloqueo} />
+        <div className="progress">
+          <p className="time-left">{showCurrentSeconds}</p>
+          <Progress progress="value" value={Math.floor(currentSeconds)} total={totalSeconds} size="tiny" className="barraProgreso" />
+          <p className="time-right">{totalCancionDB}</p>
+        </div>
 
-        <p className='time-left'>{showCurrentSeconds}</p>
-
-        {/* <Progress progress='value' value={Math.floor(currentSeconds)} total={totalSeconds} size='tiny' className='barraProgreso' /> */}
-
-        <Progress progress='value' value={Math.floor(currentSeconds)} total={totalSeconds} size='tiny' className='barraProgreso' />
-
-        {/* <p className='time-right'>{showTotalSeconds}</p> */}
-        <p className='time-right' >{totalCancionDB}</p>
+        <ReactPlayer
+            url={cancion?.urlCancion}
+            playing={play}
+            height={0}
+            width={0}
+            volume={volumen}
+            onProgress={enProgreso}
+            muted={props.mute}
+            onEnded={handleEnded}
+        />
       </div>
+  );
+};
 
-      <ReactPlayer url={cancion?.urlCancion} playing={play} height={0} width={0} volume={volumen} onProgress={enProgreso} muted={props.mute} />
-    </div>
-  )
-}
-
-export default Player
+export default Player;
