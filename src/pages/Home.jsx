c37bc81b8e44e1a24f72de431173f339cc0dc7b9
Lisandro.vargas <jose.vargas.60106@usalesiana.edu.bo>
2023-10-20 13:26:38 -0400

    Modify values to call to backend into the list of songs

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,32 +1,26 @@
-import React, {useEffect, useState} from 'react';
-import {Cancion} from "../api/Cancion"
-import "./Home.css";
+import React, { useEffect, useState } from 'react';
 import ListaCanciones from '../components/ListaCanciones';
+import {getSongList} from "../api/services/songService";
 
-const cancionController = new Cancion();
-
-export default function Home () {
-
+export default function Home() {
     const [canciones, setCanciones] = useState([]);
 
-    useEffect(() =>{
-      (async () => {
-        try {
-          
-          const response = await cancionController.obtenerTodas();
-          console.log(response);
-          setCanciones(response);
+    useEffect(() => {
+        (async () => {
+            try {
+                const response = await getSongList();
+                console.log(response);
+                setCanciones(response);
+            } catch (error) {
+                console.error(error);
+            }
+        })();
+    }, []);
 
-        } catch (error) {
-          console.error(error);
-        }
-      })();
-    },[]);
     return (
-      <div className='canciones'>
-        <h1>Lista de canciones</h1>
-
-        <ListaCanciones canciones={canciones}/>
-      </div>
+        <div className='canciones'>
+            <h1>Lista de canciones</h1>
+            <ListaCanciones canciones={canciones} />
+        </div>
     )
-  }
+}
