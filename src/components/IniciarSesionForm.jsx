fee61b210ff6a8b3a90c2b81531769a8aa9bbfa5
IsrPort <isra.pqr@gmail.com>
2023-10-23 15:56:33 -0400

    diseño y validacion de formulario de Login

--- src/components/IniciarSesionForm.jsx
+++ src/components/IniciarSesionForm.jsx
@@ -1,19 +1,82 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
-import { Button } from 'semantic-ui-react'
-
-
+import { Form, Icon, Button } from 'semantic-ui-react'
+import { useFormik } from 'formik';
+import { initialValues, validationSchema } from "./IniciarSesionForm.data"
+import "./IniciarSesionForm.css"
 
 const IniciarSesionForm = () => {
 
-  
+  const formik = useFormik({
+    initialValues: initialValues(),
+    validationSchema: validationSchema(),
+    onSubmit: async (formValue) => {
+
+      console.log("formulario enviado ....");
+      console.log(formValue);
+      // try {
+      //   await auth.register(formValue.email, formValue.password);
+      //   alert("Usuario Creado Satisfactoriamente");
+      // } catch (error) {
+      //   console.error(error);
+      // }
+
+
+    }
+  });
+
+
+
+
   return (
-    <div style={{backgroundColor: "#f0f"}}>
-      <h1>Form Inicio de Sesion</h1>
+    <div className='login-form'>
+      
+      <h1>MÚSICA GRATIS ONLINE</h1>
 
 
+      <Form onSubmit={formik.handleSubmit}>
+        <Form.Input
+
+          name="email"
+          type='text'
+          placeholder="Correo electrónico"
+          icon="mail outline"
+          onChange={formik.handleChange}
+          value={formik.values.email}
+          error={formik.errors.email}
+          
+        />
+        <Form.Input
+          name="password"
+          type='password'
+          placeholder="Contraseña"
+          icon={
+            <Icon
+              name='eye'
+              link
+              onClick={() => console.log("Show Password")}
+
+            />
+          }
+          onChange={formik.handleChange}
+          value={formik.values.password}
+          error={formik.errors.password}
+          
+        />
+      
+
+        <Form.Button type='submit' primary fluid loading={formik.isSubmitting} className='boton-iniciar-sesion'>
+          INICIAR SESIÓN
+        </Form.Button>
+      </Form>
+
+      <div className='login-form-options'>
+        <p> <Button as={Link} to="/home">Volver</Button></p>
+        
+      </div>
+{/* 
       <Button as={Link} to="/registro">Crear Cuenta</Button>
-      <Button as={Link} to="/home">Volver</Button>
+      <Button as={Link} to="/home">Volver</Button> */}
     </div>
   )
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,12 +1,29 @@
 import React from 'react'
 import IniciarSesionForm from '../components/IniciarSesionForm'
+import { Image } from 'semantic-ui-react';
+import { logo } from '../images';
+import "./Login.css"
 
 const Login = () => {
   return (
-    <div>
+    <div className='contenedor'>
+
+    <div className='contenedor-formulario-login'>
+        <Image
+            src={logo}
+            alt="Espotify"
+            className='logo'
+        />
+
       <IniciarSesionForm/>
+    
     </div>
+
+</div>
+    
+
   )
 }
 
 export default Login
+
