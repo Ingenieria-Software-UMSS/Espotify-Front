b344ba63217ced0058a8242e386d7d7dd4d3f9a2
Sergio Garcia <67697132+Sega1A@users.noreply.github.com>
2023-10-24 20:19:27 -0400

    Add files via upload
    Implementacion buscar por artista

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,32 +1,44 @@
-import React, {useEffect, useState} from 'react';
-import {Cancion} from "../api/Cancion"
+import React, {Fragment, useEffect, useState} from 'react';
+import {Cancion} from "../api/Cancion";
 import "./Home.css";
 import ListaCanciones from '../components/ListaCanciones';
+import CampoBusqueda from '../components/CampoBusqueda';
 
 const cancionController = new Cancion();
 
 export default function Home () {
-
     const [canciones, setCanciones] = useState([]);
+    const [busquedaResults, setBusquedaResults] = useState([]);
+    const [busquedaCanciones, setBusquedaCanciones] = useState([]);
 
     useEffect(() =>{
       (async () => {
         try {
-          
           const response = await cancionController.obtenerTodas();
-          console.log(response);
           setCanciones(response);
-
+          setBusquedaCanciones(response);
+          console.log(response);
         } catch (error) {
           console.error(error);
         }
       })();
     },[]);
-    return (
-      <div className='canciones'>
-        <h1>Lista de canciones</h1>
 
-        <ListaCanciones canciones={canciones}/>
-      </div>
+    return (
+      <Fragment>
+        <div>
+          <CampoBusqueda
+            busquedaResults={busquedaResults}
+            setBusquedaResults={setBusquedaResults}
+            setBusquedaCanciones={setBusquedaCanciones}
+            canciones={canciones}
+          />
+        </div>
+        <div className='canciones'>
+
+          <h1>Lista de canciones</h1>
+          <ListaCanciones canciones={busquedaCanciones}/>
+        </div>
+      </Fragment>
     )
   }
