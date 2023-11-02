0c34b6a2ec75d552b96b38cdea35d3a4ea0a56e3
Sega1A <sergio.garciacuchallo@gmail.com>
2023-11-02 17:33:21 -0400

    arreglos/correciones de buscador

--- src/components/CampoBusqueda.jsx
+++ src/components/CampoBusqueda.jsx
@@ -1,108 +1,113 @@
-import React, { useState } from 'react'
-import { Search , Modal} from 'semantic-ui-react'
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import { Search } from 'semantic-ui-react'
+import "./CampoBusqueda.css"
 
-function SearchBar({busquedaResults,setBusquedaResults,setBusquedaCanciones,canciones}){
-    const [entradaBuscador, setEntradaBuscador] = useState('');
-    // const [searchHistory, setSearchHistory] = useState([]);
-    let listaArtistas,listaCanciones;
+const CampoBusqueda = React.forwardRef(function ({busquedaResults,setBusquedaResults}, ref){
+    const [entradaBuscador, setEntradaBuscador] = React.useState('');
+    const [listaArtistas,setListaArtistas] = React.useState([]);
+    const [listaCanciones,setListaCanciones] = React.useState([]);
+    const [loading,setLoading] = React.useState(false);
+    const [enterDown,setEnterDown] = React.useState(false);
+    
+    const [id,setId] = React.useState('');
+    const [tipo,setTipo] = React.useState('');
 
-    const getArtistas = () => {
-        fetch('https://espotify.azurewebsites.net/artist')
-          .then((res) => res.json())
-          .then((json) => {
-            const objetoStringJson = JSON.stringify(json);
-            sessionStorage.setItem('jsonArtistas', objetoStringJson);
-          })
-          .catch((err) => {
-            console.log(`Error message: ${err}`);
-          });
-    };
+    const navigate = useNavigate();
 
-    const getCanciones = () => {
-        fetch('https://espotify.azurewebsites.net/song')
-          .then((res) => res.json())
-          .then((json) => {
-            const objetoStringJson = JSON.stringify(json);
-            sessionStorage.setItem('jsonCanciones', objetoStringJson);
-          })
-          .catch((err) => {
-            console.log(`Error message: ${err}`);
-          });
-    };
+    React.useEffect(() => {
+        fetch('https://espotify.azurewebsites.net/artist') //Version deploy
+        // fetch('http://localhost:8080/artist') //Version de prueba
+        .then((res) => res.json())
+        .then((dataArtistas) => {
+            setListaArtistas(dataArtistas)
+        })
+        .catch((err) => console.error(err));
 
-    listaArtistas = JSON.parse(sessionStorage.getItem('jsonArtistas'));
-    listaCanciones = JSON.parse(sessionStorage.getItem('jsonCanciones'));
+        fetch('https://espotify.azurewebsites.net/song') //Version deploy
+        // fetch('http://localhost:8080/song') //Version de prueba
+        .then((res) => res.json())
+        .then((dataCanciones) => {
+            setListaCanciones(dataCanciones)
+        })
+        .catch((err) => console.error(err));
+    },[setListaArtistas,setListaCanciones]);
 
-    const buscarResultados = (value) => {
-        if(!sessionStorage.getItem('jsonArtistas') && !sessionStorage.getItem('jsonCanciones')){
-            getArtistas();
-            getCanciones();
-        }else{    
-            const resultsSet = new Set();
-            try {
-            listaArtistas.forEach((artista) => {
-                if (artista && artista.artistName && artista.artistName.toLowerCase().includes(value.toLowerCase())) {
-                    resultsSet.add({ title: artista.artistName, description: 'Artista' });
-                }
-                });
-            listaCanciones.forEach((song) => {
-                if (song && song.songTitle && song.songTitle.toLowerCase().includes(value.toLowerCase())) {
-                    resultsSet.add({ title: song.songTitle, description: 'Cancion' });
-                }
-                });
-        
-            const res = [...resultsSet];
-            setBusquedaResults(res);
-            } catch (error) {
-                console.log(error);
+    const buscarResults = (valor) => {
+        let res = [];
+        for (let i = 0; i < listaArtistas.length; i++) {
+            // if(valor.toLowerCase() === listaArtistas[i].artistName.toLowerCase()){
+            if(listaArtistas[i].artistName.toLowerCase().includes(valor.toLowerCase())){
+                setId(listaArtistas[i].artistId);
+                setTipo('artista');
+                res.push({title: listaArtistas[i].artistName, description: 'Artista'});
             }
         }
-    };
-
-    const mostrarCanciones = (nombreArtista) => {
-        const result = canciones.filter((cancion) => {
-            return cancion.artista === nombreArtista;
-
-        });
-        
-        setBusquedaCanciones(result);
+        if(res.length === 0){
+            for (let i = 0; i < listaCanciones.length; i++) {
+                // if(valor.toLowerCase() === listaCanciones[i].songTitle.toLowerCase()){
+                // if(listaCanciones[i].songTitle.toLowerCase().includes(valor.toLowerCase())){
+                //     setId(listaCanciones[i].songId);
+                //     setTipo('cancion');
+                //     res.push({title: listaCanciones[i].songTitle, description: 'Cancion'});
+                // }
+                const songTitle = listaCanciones[i].songTitle.toLowerCase();
+                if (songTitle.includes(valor.toLowerCase()) && !res.some(item => item.title === songTitle)) {
+                    setId(listaCanciones[i].songId);
+                    setTipo('cancion');
+                    res.push({ title: songTitle, description: 'Cancion' });
+                }
+            }
+        }
+        if(res.length === 0) {
+            setId('-1');
+            setTipo('none')
+        }
+        setBusquedaResults(res);
+        setLoading(false);
+        return res;
     };
 
     const handleChange = (value) => {
+        setLoading(!loading);
         setEntradaBuscador(value);
-        buscarResultados(value);
-        if(value === ""){
-            setBusquedaCanciones(canciones);
-        }
+        buscarResults(value);
     };
 
     const selectecResult = (data) => {
         if (data && data.title) {
-            const selectedArtistName = data.title;
-            mostrarCanciones(data.title);
-            handleChange(selectedArtistName);
+            const selectedItem = data.title;
+            handleChange(selectedItem);
         }
-        // else if(data && data.songTitle){
-        //     const selectedSongTitle = data.songTitle;
-        //     handleChange(selectedSongTitle);
-        // }
-        // setSearchHistory([...searchHistory, entradaBuscador]);
+    };
+
+    const keyDown = (e) => {
+        if (e.key === 'Enter') {
+            setEnterDown(true);
+            let url = `/results?search=${entradaBuscador}&id=${id}&tipo=${tipo}`;
+            if(entradaBuscador === ''){
+                url =`/results?search=${entradaBuscador}&id=${''}&tipo=${''}`;
+            }
+            navigate(url);
+        }
     };
 
     return (
-        <div>
+        <div style={{ flexGrow: 1}}>
             <Search
-                input={{ icon: 'search', iconPosition: 'left' }}
+                input={{ icon: 'search', iconPosition: 'left', ref }}
                 value={entradaBuscador}
                 onSearchChange={(e) => handleChange(e.target.value)}
                 results={busquedaResults}
                 onResultSelect={(e, { result }) => selectecResult(result)}
-                placeholder='¿Que es lo que quieres escuchar?'
+                placeholder="¿Que es lo que quieres escuchar?"
                 showNoResults={false}
-                className='search-bar'
+                onKeyDown={keyDown}
+                loading={loading}
+                selectFirstResult={enterDown}
             />
         </div>
     );
-}
+});
 
-export default SearchBar;
+export default CampoBusqueda;
