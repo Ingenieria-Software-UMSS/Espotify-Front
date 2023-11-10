7e465597a3a3e524dbf052137a3611dec65e7ed8
IsrPort <isra.pqr@gmail.com>
2023-11-09 23:27:17 -0400

    construccion de la tabla historial de canciones

--- src/components/ListSongsHistory.jsx
+++ src/components/ListSongsHistory.jsx
@@ -1,28 +1,47 @@
 import React from 'react'
-import { Table, Icon, TableHeader } from 'semantic-ui-react';
+import { Table, Icon, TableHeader, TableBody, TableCell } from 'semantic-ui-react';
 import { size, map } from 'lodash';
 import "./ListSongsHistory.css"
 
 const ListSongsHistory = (props) => {
 
-    const {songs} = props;
+    const { songs } = props;
 
-    if(size(songs)===0){
+    if (size(songs) === 0) {
         return <p className='no-songs'>No hay canciones en el historial</p>;
     }
 
 
-  return (
-    <Table inverted className='list-songs'>
-        <TableHeader>
-            <Table.Row>
-                <Table.HeaderCell/>
-                <Table.HeaderCell>Titulo</Table.HeaderCell>
-            </Table.Row>
-        </TableHeader>
+    return (
+        <div className='tabla-contenedor'>
+            <div className='tabla-historial'>
+                <Table inverted className='list-songs'>
+                    <TableHeader>
+                        <Table.Row>
+                            <Table.HeaderCell />
+                            <Table.HeaderCell>Titulo</Table.HeaderCell>
+                        </Table.Row>
+                    </TableHeader>
 
-    </Table>
-  )
+                    <TableBody>
+                        {map(songs, (song) => (
+                            <Table.Row key={song.id}>
+                                <Table.Cell collapsing>
+                                    <Icon name='play circle outline' />
+                                </Table.Cell>
+
+                                <TableCell>{song.nombre}</TableCell>
+                            </Table.Row>
+                        ))}
+                    </TableBody>
+
+
+                </Table>
+            </div>
+        </div>
+
+
+    )
 }
 
 export default ListSongsHistory
