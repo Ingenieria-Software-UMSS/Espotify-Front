9cb96284403aef7271c2855ea3d94bf20415c03c
dagac <qtimpo1@gmail.com>
2023-11-26 18:59:27 -0400

    bugs gestion lista

--- src/components/Informacion.jsx
+++ src/components/Informacion.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Informacion.css';
 import { Button } from 'semantic-ui-react';
 import { Dropdown } from 'semantic-ui-react';
@@ -19,7 +19,7 @@ const Informacion = () => {
 
     return (
         <div className='informacion'>
-            <div className='boton-contenedor'>
+            <div className='boton-contenedor-regresar'>
                 <Link to='/home'>
                     <Button circular icon='left chevron' className='boton-regresar' />
                 </Link>
@@ -50,40 +50,46 @@ const DropdownListaReproduccion = (props) => {
     const [listas, setListas] = React.useState([]);
 
     React.useEffect(() => {
-        fetch("https://espotify.azurewebsites.net/play-list")
-        // fetch("http://localhost:8080/play-list")
-            .then((res) => res.json())
-            .then((json) => {
-                setListas(json);
-            });
-    }, [setListas]);
+        const token = localStorage.getItem('auth_token');
+        console.log(token);
 
-    const [listasExistentes, setListasExistentes] = React.useState([
-        {
-            id: 1,
-            nombre: 'Lista 1'
-        },
-        {
-            id: 2,
-            nombre: 'Lista 2'
-        },
-        {
-            id: 3,
-            nombre: 'Lista 3'
-        }
-    ]);
-    const hayListas = listasExistentes.length > 0;
+        fetch('https://espotify.azurewebsites.net/play-list', {
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${token}`,
+                'Content-Type': 'application/json'
+            }
+        })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Error en la solicitud');
+                }
+                console.log("status", response.status, response.statusText)
+                return response.json();
+            })
+            .then(data => {
+                setListas(data);
+            })
+            .catch(error => {
+                console.error('Hubo un error:', error);
+            });
+    }, []);
+
     const navigate = useNavigate();
     const [idLista, setIdLista] = React.useState(null);
 
     const anadirPlayList = (playListId) => {
         // return fetch("http://localhost:8080/play-list-song", {
-            return fetch("https://espotify.azurewebsites.net/play-list-song", {
+        const token = localStorage.getItem('auth_token');
+
+        return fetch("https://espotify.azurewebsites.net/play-list-song", {
 
             method: "POST", // *GET, POST, PUT, DELETE, etc.
             mode: "cors", // no-cors, *cors, same-origin
 
             headers: {
+                'Authorization': `Bearer ${token}`,
+
                 "Content-Type": "application/json",
             },
             body: JSON.stringify({
@@ -100,74 +106,69 @@ const DropdownListaReproduccion = (props) => {
     return (
         <>
             {
-                hayListas && (
-                    // y esta logeado
-                    <Dropdown icon='plus large'>
-                        <Dropdown.Menu className='dropdownmenu'>
-                            <Dropdown.Header content='Listas exitentes' />
-                            <Dropdown.Divider />
-                            {
-                                // listasExistentes.map((listaExistente) => {
-                                listas.map((listaExistente) => {
-                                    return (
+                <Dropdown icon='plus big circle' upward>
+                    <Dropdown.Menu className='dropdownmenu'>
+                        <Dropdown.Header content='Listas exitentes' />
+                        <Dropdown.Divider />
+                        {
+                            listas.map((listaExistente) => {
+                                return (
+                                    <Dropdown.Item
+                                        key={listaExistente.playListId}
+                                        text={listaExistente.playListName}
+                                        className='dropdownItem'
+                                        direction='up'
+                                        onClick={() => {
+                                            console.log("listas existentes", listaExistente);
+                                            console.log("listas", listas);
 
-                                        <Dropdown.Item
-                                            key={listaExistente.playListId}
-                                            text={listaExistente.playListName}
-                                            onClick={() => {
-                                                console.log("listas existentes", listaExistente);
-                                                console.log("listas", listas);
-                                                
-                                                // comprobar si ya existe en la playlist
-                                                const playListSongList = listaExistente.playListSongList;
-                                                let ocurrencias = 0;
-                                                for (let i = 0; i < playListSongList.length; i++) {
+                                            // comprobar si ya existe en la playlist
+                                            const playListSongList = listaExistente.playListSongList;
+                                            let ocurrencias = 0;
+                                            for (let i = 0; i < playListSongList.length; i++) {
 
-                                                    if (playListSongList[i].song.songId === parseInt(props.songId)) {
-                                                        ocurrencias++;
+                                                if (playListSongList[i].song.songId === parseInt(props.songId)) {
+                                                    ocurrencias++;
+                                                }
+                                            }
+                                            if (ocurrencias === 0) {
+                                                //añade
+
+                                                anadirPlayList(listaExistente.playListId).then((res) => {
+                                                    if (res.ok) {
+                                                        setModalAgregadoOpen(true);
+                                                        setIdLista(listaExistente.playListId);
+                                                    } else {
+                                                        console.log(idLista, "else");
                                                     }
-                                                }
-                                                if (ocurrencias === 0) {
-                                                    //añade
-
-                                                    anadirPlayList(listaExistente.playListId).then((res) => {
-                                                        if (res.ok) {
-                                                            setModalAgregadoOpen(true);
-                                                            setIdLista(listaExistente.playListId);
-                                                        } else {
-                                                            console.log(idLista, "else");
-                                                        }
-                                                    })
+                                                })
 
 
-                                                }
-                                                if (ocurrencias === 1) {
-                                                    //modal
-                                                    setModalRepetidoOpen(true);
-                                                    setIdLista(listaExistente.playListId);
-                                                    setNombreLista(listaExistente.playListName);
-
-                                                }
-
-                                                if (ocurrencias >= 2) {
-                                                    alert("elemento  existente")
-                                                }
-
-                                            }}
-                                        />
-                                    )
-                                })
-                            }
-                        </Dropdown.Menu>
-                    </Dropdown>)
+                                            }
+                                            if (ocurrencias === 1) {
+                                                //modal
+                                                setModalRepetidoOpen(true);
+                                                setIdLista(listaExistente.playListId);
+                                                setNombreLista(listaExistente.playListName);
+                                            }
+                                            
+                                            if (ocurrencias >= 2) {
+                                                alert("elemento  existente")
+                                            }
+                                        }}
+                                    />
+                                )
+                            })
+                        }
+                    </Dropdown.Menu>
+                </Dropdown>
             }
             <ModalAgregadoExito
-                open={modalAgregadoOpen} 
+                open={modalAgregadoOpen}
                 onClose={() => {
                     setModalAgregadoOpen(false);
                 }}
                 onAccept={() => {
-
                     setModalAgregadoOpen(false);
                     const url = `/playlist/${idLista}`;
                     navigate(url);
@@ -180,11 +181,11 @@ const DropdownListaReproduccion = (props) => {
                 onClose={() => {
                     setModalRepetidoOpen(false);
                 }}
-                onAgregarDTM ={() => {
+                onAgregarDTM={() => {
                     anadirPlayList(idLista).then((res) => {
                         if (res.ok) {
                             setModalAgregadoOpen(true);
-                            } else {
+                        } else {
                             console.log(idLista, "else");
                         }
                     })
@@ -195,8 +196,6 @@ const DropdownListaReproduccion = (props) => {
 }
 
 const ModalAgregadoExito = (props) => {
-    // const [modalAgregadoOpen, setModalAgregadoOpen] = React.useState(false);
-
     return (
         <>
             <Modal
@@ -224,17 +223,20 @@ const ModalAgregadoRepetido = (props) => {
             <Modal
                 open={props.open}
                 onClose={props.onClose}
+                size='small'
             >
                 <Modal.Content className='modalRepetido_contenido'>
                     <div className='mensaje'>
                         <h1>El elemento ya está agregado en la playlist "{props.nombreLista}".</h1>
                     </div>
-                    <Button onClick={props.onAgregarDTM} className='modal_botonAgregarDTM' color='gray' >
-                        Agregar de todos modos
-                    </Button>
-                    <Button onClick={props.onClose} className='modal_botonNoAgregar' color='black'>
-                        No agregar
-                    </Button>
+                    <div className='contenedor_botones_ADTM' >
+                        <Button onClick={props.onAgregarDTM} className='modal_botonAgregarDTM' color='gray' >
+                            Agregar de todos modos
+                        </Button>
+                        <Button onClick={props.onClose} className='modal_botonNoAgregar' color='black'>
+                            No agregar
+                        </Button>
+                    </div>
                 </Modal.Content>
             </Modal>
         </>
